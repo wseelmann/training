c8295d871a8c9476ff1ea38a329e0294c1fbc8f1
Alex Kellner <alexander.kellner@in2code.de>
2018-02-20 14:30:12 +0100

    [TASK] Extend filter with another field (from language)

--- typo3conf/ext/translation/Classes/Domain/Repository/TransRepository.php
+++ typo3conf/ext/translation/Classes/Domain/Repository/TransRepository.php
@@ -1,6 +1,7 @@
 <?php
 namespace In2code\Translation\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
@@ -17,20 +18,42 @@ class TransRepository extends Repository
     public function findByFilter(array $filter)
     {
         $query = $this->createQuery();
-
-        if ($filter !== []) {
-            $logicalOr = [
-                $query->like('customer', '%' . $filter['searchword'] . '%'),
-                $query->like('fromLanguage', '%' . $filter['searchword'] . '%'),
-                $query->like('toLanguage', '%' . $filter['searchword'] . '%'),
-                $query->like('subjectFrom', '%' . $filter['searchword'] . '%'),
-                $query->like('subjectTo', '%' . $filter['searchword'] . '%')
-            ];
-            $query->matching(
-                $query->logicalOr($logicalOr)
-            );
-        }
-
+        $this->addFilterQuery($filter, $query);
         return $query->execute();
+    }
+
+    /**
+     * @param array $filter
+     * @param QueryInterface $query
+     * @return void
+     */
+    protected function addFilterQuery(array $filter, QueryInterface $query)
+    {
+        if ($this->isAnyFilterSet($filter)) {
+            $logicalAnd = [];
+            if (!empty($filter['searchword'])) {
+                $logicalOr = [
+                    $query->like('customer', '%' . $filter['searchword'] . '%'),
+                    $query->like('fromLanguage', '%' . $filter['searchword'] . '%'),
+                    $query->like('toLanguage', '%' . $filter['searchword'] . '%'),
+                    $query->like('subjectFrom', '%' . $filter['searchword'] . '%'),
+                    $query->like('subjectTo', '%' . $filter['searchword'] . '%')
+                ];
+                $logicalAnd[] = $query->logicalOr($logicalOr);
+            }
+            if (!empty($filter['from'])) {
+                $logicalAnd[] = $query->equals('fromLanguage', $filter['from']);
+            }
+            $query->matching($query->logicalAnd($logicalAnd));
+        }
+    }
+
+    /**
+     * @param array $filter
+     * @return bool
+     */
+    protected function isAnyFilterSet(array $filter): bool
+    {
+        return !empty($filter['searchword']) || !empty($filter['from']);
     }
 }
